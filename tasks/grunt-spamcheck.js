e24a9be5c48cea369a48ce67fa9e5fd4cd417e4a
Derek Rushforth <derek@wildbit.com>
2014-12-15 22:01:30 -0700

    Add jshint recipe, change template to text only, cleanup

--- tasks/grunt-spamcheck.js
+++ tasks/grunt-spamcheck.js
@@ -9,43 +9,43 @@ var spamcheck = require('../lib/postmark-spamcheck');
 
 module.exports = function(grunt) {
 
-  grunt.registerMultiTask('spamcheck', 'Spam check your emails using Postmark\'s API', function() {
+	grunt.registerMultiTask('spamcheck', 'Spam check your emails using Postmark\'s API', function() {
 
-    var done = this.async(),
-        options = this.options(),
-        _data = this.data;
+		var done = this.async(),
+				options = this.options(),
+				_data = this.data;
 
-    if (this.files.length > 0) {
+		if (this.files.length > 0) {
 
-    	// TODO: validate options
-    	// TODO: insert defaults
+			// TODO: validate options
+			// TODO: insert defaults
 
-    	var requestOptions = {
-        options: _data.options || options.options
-      };
+			var requestOptions = {
+				options: _data.options || options.options
+			};
 
-    	if (this.filesSrc.length > 1) {
-    		// TODO: support batch requests for multiple emails
-      } else {
-        requestOptions.email = grunt.file.read(this.filesSrc);
+			if (this.filesSrc.length > 1) {
+				// TODO: support batch requests for multiple emails
+			} else {
+				requestOptions.email = grunt.file.read(this.filesSrc);
 
-        spamcheck.getScore(requestOptions, function(err, response) {
+				spamcheck.getScore(requestOptions, function(err, response) {
 					if (err) {
-            grunt.log.warn('Error response: ' + JSON.stringify(err));
-          } else {
-            grunt.log.writeln('Results:');
-            grunt.log.writeln(JSON.stringify(response));
-          }
-          done();
-        });
+						grunt.log.warn('Error response: ' + JSON.stringify(err));
+					} else {
+						// TODO: colorize score based on number
+						grunt.log.writeln('Spam check score: ✓ ' + response.score);
+					}
+					done();
+				});
 
-      }
-    
-    } else {
-      grunt.log.warn('No src file found \n');
-    }
+			}
+		
+		} else {
+			grunt.log.warn('No src file found \n');
+		}
 
-  });
+	});
 
 
 };
